dd4b5024cc84a04487fa59bca9df6fe78fedd7c0
VioTerra <72231969+VioTerra@users.noreply.github.com>
2023-02-15 23:02:31 +0300

    v1.0
    Дз-1, 4 и 5 глава

---
+++ 4.1.py
@@ -0,0 +1,12 @@
+# Задание 4.1
+
+# Используя подготовленную строку nat, получить новую строку,
+# в которой в имени интерфейса вместо FastEthernet написано GigabitEthernet.
+# Полученную новую строку вывести на стандартный поток вывода (stdout) с помощью print.
+
+# Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+nat = nat.replace('Fast', 'Gigabit')
+print(nat)
